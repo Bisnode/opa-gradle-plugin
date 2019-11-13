977081b03072e79e90c6447bc32d4bef81050345
Anders Eknert <anders.eknert@bisnode.com>
2019-11-13 11:50:33 +0100

    Change to package-private

--- src/main/java/com/bisnode/opa/OpaPluginExtension.java
+++ src/main/java/com/bisnode/opa/OpaPluginExtension.java
@@ -5,27 +5,27 @@ public class OpaPluginExtension {
     private String srcDir = "src/main/rego";
     private String testDir = "src/test/rego";
 
-    public String getLocation() {
+    String getLocation() {
         return location;
     }
 
-    public void setLocation(String location) {
+    void setLocation(String location) {
         this.location = location;
     }
 
-    public String getSrcDir() {
+    String getSrcDir() {
         return srcDir;
     }
 
-    public void setSrcDir(String srcDir) {
+    void setSrcDir(String srcDir) {
         this.srcDir = srcDir;
     }
 
-    public String getTestDir() {
+    String getTestDir() {
         return testDir;
     }
 
-    public void setTestDir(String testDir) {
+    void setTestDir(String testDir) {
         this.testDir = testDir;
     }
 }
